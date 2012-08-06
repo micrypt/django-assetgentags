20240e25cdbef2dca62341eb64e9900e41476ff2
Seyi Ogunyemi <micrypt@gmail.com>
2012-08-06 14:28:11 +0200

    Update assetgentags/templatetags/assetgentags.py

--- assetgentags/templatetags/assetgentags.py
+++ assetgentags/templatetags/assetgentags.py
@@ -4,15 +4,10 @@ from django import template
 from django.conf import settings
 from django.core.exceptions import ImproperlyConfigured
 
-try:
-    from settings import ASSETS_JSON
-except:
-    raise ImproperlyConfigured("Please specify an ASSETS_JSON path in your settings")
-
 register = template.Library()
 
 def use(p, func):
   with open(p) as f:
     return func(f)
 
-register.simple_tag(lambda x: use(ASSETS_JSON, json.load)[x], name="load_asset")
+register.simple_tag(lambda x: use(settings.ASSETS_JSON, json.load)[x], name="load_asset")
